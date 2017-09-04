24475701a2e7cad802ac8521051f9e2fada5f0e8
Gzw <gzw102030@outlook.com>
2017-09-04 11:41:40 +0800

    1.0 最终版提交

---
+++ app/src/main/java/com/jaaaelu/gzw/neteasy/privatebook/activities/WebViewActivity.java
@@ -0,0 +1,15 @@
+package com.jaaaelu.gzw.neteasy.privatebook.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jaaaelu.gzw.neteasy.privatebook.R;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+    }
+}
---
+++ common/src/main/java/com/jaaaelu/gzw/neteasy/common/widget/PercentPieView.java
@@ -0,0 +1,8 @@
+package com.jaaaelu.gzw.neteasy.common.widget;
+
+/**
+ * Created by Gzw on 2017/9/1 0001.
+ */
+
+public class PercentPieView {
+}
